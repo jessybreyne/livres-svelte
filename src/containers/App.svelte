cc9c4969ce9801f829c1c4c10ead0a1613a6b57b
Alexandre Guibreteau <aguibreteau@info03-24.iut45.univ-orleans.fr>
2020-01-21 12:01:06 +0100

    Implémentation du composant pour la barre de recherche

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import DB from "../components/DB.svelte";
-  import Pagination from "./Pagination.svelte"
+  import Pagination from "./Pagination.svelte";
+  import Search from "./Search.svelte";
   import "@polymer/paper-card";
   import "@material/mwc-button";
   import { Col, Container, Row } from "sveltestrap";
@@ -17,6 +18,8 @@
    <mwc-top-app-bar>
     <mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
     <div slot="title">Books Storage</div>
+    <Search bind:items={books}/>
+
     <mwc-icon-button icon="file_download" slot="actionItems"></mwc-icon-button>
     <mwc-icon-button icon="print" slot="actionItems"></mwc-icon-button>
     <mwc-icon-button icon="favorite" slot="actionItems"></mwc-icon-button>
