341b044af5b15f049f24729d734d3ca3785d0e4a
Alexandre Guibreteau <aguibreteau@info03-24.iut45.univ-orleans.fr>
2020-01-21 12:29:17 +0100

    Algo recherche

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,17 +1,24 @@
 <script>
-   import { FormGroup, Input } from "sveltestrap";
-   export let items;
-
+    import { FormGroup, Input } from "sveltestrap";
+    export let items;
+    let query;
 
  async function search(query, items) {
-   
+    let result = [];
+    for(let book in items){
+        if(book.title.search(query) || book.author.search(query)){
+            result.append(book);
+        }
+    }
+    console.log(result);
  }
 </script>
 
 <FormGroup>
     <Input
-      type="search"
-      name="search"
-      id="exampleSearch"
-      placeholder="search placeholder" />
+        bind:value={query}
+        type="search"
+        name="search"
+        id="exampleSearch"
+        placeholder="search placeholder" />
   </FormGroup>
